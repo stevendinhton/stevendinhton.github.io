bc7d0f1595c35751088198a7b729895ef323fa93
unknown <stevendinhton@gmail.com>
2019-02-12 10:13:37 -0500

    test http request stuff

---
+++ js/send.js
@@ -0,0 +1,18 @@
+function getIP(json) {
+	xhr = new XMLHttpRequest();
+	var url = "http://10.88.240.235:8080" + "/" + json.ip;
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function () { 
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var json = JSON.parse(xhr.responseText);
+			console.log(json.email + ", " + json.name)
+		}
+	}
+	xhr.send(data);
+}
+
+
+
+
+
